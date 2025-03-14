baee754972191c03a113b03dd462958380e490b5
Hung-Yi <hung-yi.li@quantatw.com>
2025-03-14 13:39:47 +0800

    aalc: add fan table R6
    Summary:
    - add fan table R6
    - support fsc temp calculation to decimals
    - disable rpu inlet temp(0x84) failure behavior
    - modify pressure sensor retry to 3
    - modify fault led status bit from 5 to 3
    - support modbus multi write/read
    - add 10ms delay to set sticky sensor status
    - add force init of fw update flag
    - enable otp
    - add abr relate function
    - add 10ms delay before fru write
    - fix BPB HSC fail log rewriting issue
    
    Test Plan:
    - Build code: Pass

--- meta-facebook/aalc-rpu/src/platform/plat_util.c
+++ meta-facebook/aalc-rpu/src/platform/plat_util.c
@@ -29,6 +29,7 @@
 #include "plat_pwm.h"
 #include "plat_fsc.h"
 #include "plat_hook.h"
+#include <shell/shell_uart.h>
 #include <logging/log_ctrl.h>
 
 #define I2C_MASTER_READ_BACK_MAX_SIZE 16 // 16 registers
@@ -198,6 +199,28 @@ uint8_t get_fsc_mode()
 		return FSC_MODE_SEMI_MODE;
 	else
 		return FSC_MODE_UNKNOW;
+}
+
+bool get_abr(void)
+{
+	// check SCU510[11][12][13] is 1
+	return ((sys_read32(0x7e6e2510) & 0x3800) == 0x3800);
+}
+
+void set_abr(uint8_t onoff)
+{
+	char cmd[20];
+
+	sprintf(cmd, "otp pb strap 0x2b %d", (onoff ? 1 : 0));
+	shell_execute_cmd(shell_backend_uart_get_ptr(), cmd);
+
+	sprintf(cmd, "otp pb strap 0x2c %d", (onoff ? 1 : 0));
+	shell_execute_cmd(shell_backend_uart_get_ptr(), cmd);
+
+	sprintf(cmd, "otp pb strap 0x2d %d", (onoff ? 1 : 0));
+	shell_execute_cmd(shell_backend_uart_get_ptr(), cmd);
+
+	shell_execute_cmd(shell_backend_uart_get_ptr(), "");
 }
 
 #endif // PLAT_UTIL_H
