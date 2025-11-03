730b3ff24b1e3cc91820b93d06294a13bb17eb80
Kevin Huang <KevinHuang4@quntatw.com>
2025-11-03 14:30:58 +0800

    sb: rb: update sensor threshold and some new funcitons
    Summary:
    - Update new vr threshold
    - add LEAK2 trigger funciton(EVB FAB2)
    - add ASIC die temperature sensor LED control funciton
    - remove iris_power steps_on shell command debug message
    - modify ALL_VR_PM_ALERT_R_N trigger funciton execute immediately when trigger
    - when asic is updating it needs to turn off cpld RESET register all pins and restore when update is finished
    
    Test Plan:
    - Build code: Pass

--- meta-facebook/sb-rb/src/platform/plat_init.c
+++ meta-facebook/sb-rb/src/platform/plat_init.c
@@ -30,6 +30,7 @@
 #include "plat_log.h"
 #include "plat_user_setting.h"
 #include "plat_pldm_monitor.h"
+#include "plat_hwmon.h"
 
 LOG_MODULE_REGISTER(plat_init);
 
@@ -46,6 +47,7 @@ void pal_pre_init()
 	plat_led_init();
 	vr_mutex_init();
 	pwr_level_mutex_init();
+	init_pwm_dev();
 }
 
 void pal_set_sys_status()
@@ -64,6 +66,18 @@ void pal_post_init()
 	plat_write_cpld(CPLD_OFFSET_POWER_CLAMP, &data);
 	plat_adc_init();
 	init_load_eeprom_log();
+	// if board id >= EVB EVT2(FAB2)
+	if (get_asic_board_id() == ASIC_BOARD_ID_EVB && get_board_rev_id() >= REV_ID_EVT1B) {
+		init_U200051_IO();
+		quick_sensor_poll_init();
+	}
+	// if board id == EVB
+	if (get_asic_board_id() == ASIC_BOARD_ID_EVB) {
+		init_pwm_dev();
+		ast_pwm_set(65, PWM_PORT1);
+		ast_pwm_set(65, PWM_PORT6);
+	}
+
 	init_cpld_polling();
 	plat_telemetry_table_init();
 }
