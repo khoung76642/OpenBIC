6fcc7e374db66e6c8760dd10af5b736d8e16e44c
Kevin Huang <KevinHuang4@quntatw.com>
2025-11-06 19:09:28 +0800

    tmp

--- meta-facebook/sb-rb/src/shell/shell_ads7066.c
+++ meta-facebook/sb-rb/src/shell/shell_ads7066.c
@@ -69,6 +69,43 @@ static int ads7066_write_reg(uint8_t *data, uint8_t cmd, uint8_t input)
 	return 0;
 }
 
+static int ads7066_read_return(const struct shell *shell, uint8_t *data, uint8_t cmd)
+{
+	spi_dev = device_get_binding("SPIP");
+	if (!spi_dev) {
+		LOG_ERR("SPI device not find");
+	}
+
+	uint8_t tx_buf[3] = { 0x00, 0x00 };
+	uint8_t rx_buf[3] = { 0 };
+	uint8_t out_buf[48] = { 0 };
+
+	struct spi_buf tx = { .buf = tx_buf, .len = sizeof(tx_buf) };
+	struct spi_buf rx = { .buf = rx_buf, .len = sizeof(rx_buf) };
+	struct spi_buf_set tx_set = { .buffers = &tx, .count = 1 };
+	struct spi_buf_set rx_set = { .buffers = &rx, .count = 1 };
+
+	for (int i=0; i<16; i++) {
+		memset(rx_buf, 0, 3);
+		if (tx_buf[2] == 0) {
+			tx_buf[2] = cmd;
+		} else {
+			tx_buf[2] = 0x00;
+		}
+		int ret = spi_transceive(spi_dev, &spi_cfg, &tx_set, &rx_set);
+		if (ret < 0) {
+			LOG_ERR("SPI failed: %d", ret);
+			return ret;
+		}
+		memcpy(&out_buf[i*3], rx_buf, 3);
+		//shell_hexdump(shell, rx_buf, 3);
+		k_msleep(20);
+
+	}
+	shell_hexdump(shell, out_buf, 48);
+	return 0;
+}
+
 static int ads7066_read_m(const struct shell *shell, uint8_t cmd, uint8_t delay)
 {
 	spi_dev = device_get_binding("SPIP");
@@ -130,6 +167,13 @@ static int cmd_ads7066_id(const struct shell *shell, size_t argc, char **argv)
 		int ret = ads7066_read_m(shell, cmd, input);
 		if (ret < 0) {
 			shell_error(shell, "read m (err=%d)", ret);
+			return ret;
+		}
+	}
+	  else if (op == 3) {
+		int ret = ads7066_read_return(shell, data, cmd);
+		if (ret < 0) {
+			shell_error(shell, "read return (err=%d)", ret);
 			return ret;
 		}
 	}
