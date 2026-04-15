12c2c72822e1f449813f92cc63722aae3a099f0d
khoung76642 <w8989019@gmailcom>
2026-04-15 11:11:56 +0800

    modify HAMSA_VDD bus to 2a

--- meta-facebook/sb-vpd/src/platform/plat_pldm_sensor.c
+++ meta-facebook/sb-vpd/src/platform/plat_pldm_sensor.c
@@ -5250,7 +5250,7 @@ pldm_sensor_info plat_pldm_sensor_vr_table[] = {
 		{
 			.num = SENSOR_NUM_ASIC_P0V85_HAMSA_VDD_TEMP_C,
 			.type = sensor_dev_mp29816a,
-			.port = I2C_BUS3,
+			.port = I2C_BUS2,
 			.target_addr = ASIC_P0V85_HAMSA_VDD_ADDR,
 			.offset = PMBUS_READ_TEMPERATURE_1,
 			.access_checker = is_vr_access,
@@ -5322,7 +5322,7 @@ pldm_sensor_info plat_pldm_sensor_vr_table[] = {
 		{
 			.num = SENSOR_NUM_ASIC_P0V85_HAMSA_VDD_VOLT_V,
 			.type = sensor_dev_mp29816a,
-			.port = I2C_BUS3,
+			.port = I2C_BUS2,
 			.target_addr = ASIC_P0V85_HAMSA_VDD_ADDR,
 			.offset = PMBUS_READ_VOUT,
 			.access_checker = is_vr_access,
@@ -5394,7 +5394,7 @@ pldm_sensor_info plat_pldm_sensor_vr_table[] = {
 		{
 			.num = SENSOR_NUM_ASIC_P0V85_HAMSA_VDD_CURR_A,
 			.type = sensor_dev_mp29816a,
-			.port = I2C_BUS3,
+			.port = I2C_BUS2,
 			.target_addr = ASIC_P0V85_HAMSA_VDD_ADDR,
 			.offset = PMBUS_READ_IOUT,
 			.access_checker = is_vr_access,
@@ -5466,7 +5466,7 @@ pldm_sensor_info plat_pldm_sensor_vr_table[] = {
 		{
 			.num = SENSOR_NUM_ASIC_P0V85_HAMSA_VDD_INPUT_VOLT_V,
 			.type = sensor_dev_mp29816a,
-			.port = I2C_BUS3,
+			.port = I2C_BUS2,
 			.target_addr = ASIC_P0V85_HAMSA_VDD_ADDR,
 			.offset = PMBUS_READ_VIN,
 			.access_checker = is_vr_access,
@@ -8861,7 +8861,7 @@ pldm_sensor_info plat_pldm_sensor_quick_vr_table[] = {
 		{
 			.num = SENSOR_NUM_ASIC_P0V85_HAMSA_VDD_PWR_W,
 			.type = sensor_dev_mp29816a,
-			.port = I2C_BUS3,
+			.port = I2C_BUS2,
 			.target_addr = ASIC_P0V85_HAMSA_VDD_ADDR,
 			.offset = PMBUS_READ_POUT,
 			.access_checker = is_vr_access,
