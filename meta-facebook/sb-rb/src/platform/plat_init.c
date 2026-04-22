653987f546b1f1452f4290ee3b5319c7fb598919
Kevin Huang <KevinHuang4@quntatw.com>
2026-04-22 10:23:05 +0800

    add set vr test mode in the beginning

--- meta-facebook/sb-rb/src/platform/plat_init.c
+++ meta-facebook/sb-rb/src/platform/plat_init.c
@@ -106,6 +106,11 @@ void pal_post_init()
 		asic_thermtrip_error_log(LOG_ASSERT);
 	// check clk 312.5Mhz init
 	check_312_5MHz_init_status();
+	if (is_mb_dc_on() == true)
+	{
+		LOG_INF("detect dc on, start to init vr test mode");
+		vr_test_mode_enable(true);
+	}
 }
 
 #define DEF_PROJ_GPIO_PRIORITY 78
