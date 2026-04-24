0e306e633529f8e925f3104cd6a9a02224277eda
Kevin Huang <KevinHuang4@quntatw.com>
2026-04-24 10:54:58 +0800

    fix init perm volt setting 2026.21.b6

--- meta-facebook/sb-rb/src/platform/plat_init.c
+++ meta-facebook/sb-rb/src/platform/plat_init.c
@@ -38,6 +38,7 @@
 #include "plat_vr_test_mode.h"
 #include "plat_power_capping.h"
 #include "plat_isr.h"
+#include "shell_iris_power.h"
 
 LOG_MODULE_REGISTER(plat_init);
 
@@ -106,10 +107,11 @@ void pal_post_init()
 		asic_thermtrip_error_log(LOG_ASSERT);
 	// check clk 312.5Mhz init
 	check_312_5MHz_init_status();
-	if (is_mb_dc_on() == true)
-	{
+	if (is_mb_dc_on() == true) {
 		LOG_INF("detect dc on, start to init vr test mode");
 		vr_test_mode_enable(true);
+		if (!set_all_vout_command())
+			LOG_ERR("set all vout command fail!");
 	}
 }
 
