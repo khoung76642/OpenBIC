357f54646b9e4e170700f53e0e106fd2dbf6d2f6
Kevin Huang <KevinHuang4@quntatw.com>
2026-08-03 10:25:11 +0800

    sb: vpd: modified board rev_id from 3bit to 2bit

--- meta-facebook/sb-vpd/src/platform/plat_class.c
+++ meta-facebook/sb-vpd/src/platform/plat_class.c
@@ -174,8 +174,8 @@ void init_plat_config()
 	uint8_t module = 0;
 	plat_read_cpld(CPLD_OFFSET_VR_VENDER_TYPE, &module, 1);
 	plat_read_cpld(CPLD_OFFSET_BOARD_REV_ID, &board_rev_id, 1);
-	// rev id only support 0, 1, 2 bit
-	board_rev_id = board_rev_id & 0x07;
+	// rev id only support 0, 1 bit
+	board_rev_id = board_rev_id & 0x03;
 	vr_module = (module & 0x01);
 	ubc_module = (module >> 1) & 0x07;
 	uint8_t board_id = 0;
